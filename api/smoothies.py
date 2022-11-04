cd39f368ac201c9450c13709e91de0a0d2e6c5eb
MScott1607 <ayamasa.sg@protonmail.com>
2022-11-04 17:48:55 +0000

    integrated smoothie search with app.py, updated smoothies.html, search function now working

--- api/smoothies.py
+++ api/smoothies.py
@@ -1,36 +1,35 @@
-import json
 import requests
 import config
 
 
 class SmoothieManager:
+    """manages calls to edamam API"""
+    edamam_app_id = config.app_id
+    edamam_app_key = config.app_key
 
-    def get_smoothie_data(ingredient):
+    def get_smoothie_data(self, ingredient):
         """
         gets all data from the API endpoint
         """
-        api_response = requests.get(
-                f'https://api.edamam.com/api/recipes/v2?type=public'
-                f'&q={ingredient}'
-                f'&app_id={config.app_id}'
-                f'&app_key={config.app_key}'
-                f'&diet=low-fat'
-                f'&tag=smoothie')
-        return api_response.json()['hits']
+        try:
+            api_response = requests.get(
+                    f'https://api.edamam.com/api/recipes/v2?type=public'
+                    f'&q={ingredient}'
+                    f'&app_id={self.edamam_app_id}'
+                    f'&app_key={self.edamam_app_key}'
+                    f'&diet=low-fat'
+                    f'&tag=smoothie')
+            return api_response.json()['hits']
+        except (requests.exceptions.RequestException, KeyError):
+            return []
 
-
-    def smoothie_search(ingredient):
+    def smoothie_search(self, ingredient):
         """This takes an ingredient as user input and searches the API
             then it will return results"""
-        smoothie_data = SmoothieManager.get_smoothie_data(ingredient)
-        if len(smoothie_data):
-            for item in smoothie_data:
-                items = item['recipe']
-                result_values = items['label'], items['uri'], 'DIETARY LABELS: ', items[
-                    'dietLabels']
-                print(*result_values, sep='\n')
-        else:
-            print("No results found")
-
-
-#SmoothieManager.smoothie_search('peach')
+        smoothie_data = self.get_smoothie_data(ingredient)
+        return [{
+            'name': item['recipe']['label'],
+            'url': item['recipe']['url'],
+            'source': item['recipe']['source'],
+            'diet_labels': ', '.join(item['recipe']['dietLabels'])
+        } for item in smoothie_data]
