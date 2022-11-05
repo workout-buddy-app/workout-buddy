6da9395e1dd038f747d9e6d261639dbedc248aa8
Lynsay Dunne <lynsay.dunne@gmail.com>
2022-11-05 16:33:39 +0000

    unfinished smoothies test and updated About page in HTML

--- tests/test_smoothies.py
+++ tests/test_smoothies.py
@@ -4,21 +4,24 @@ from api.smoothies import SmoothieManager
 
 class TestRecipes(TestCase):
 
-    # def test_get_recipe_data_for_ingredient(self):
-    #     test_ingredient = 'mushrooms'
-    #     recipe_data = Recipes.get_recipe_data_for_ingredient(test_ingredient)
-    #     # Assert that there is at least one recipe
-    #     self.assertGreater(len(recipe_data), 0)
-    #     # Assert that at least one recipe contains the test ingredient in its title
-    #     try:
-    #         next(filter(lambda recipe: test_ingredient in recipe['recipe']['label'], recipe_data))
-    #     except StopIteration:
-    #         self.fail('Test ingredient not in any of the recipe titles')
+    # def get_smoothie_data(self):
 
-    def test_smoothie_search_good(self):
-        with mock.patch("builtins.input", side_effect=["banana"]):
-            with self.assertEqual(items['banana']):
-                smoothie_search()
+    def test_smoothie_search(self):
+        smoothie_manager = SmoothieManager()
+        test_ingredient = 'banana'
+        smoothie_data = smoothie_manager.get_smoothie_data(test_ingredient)
+        # Assert that there is at least one recipe
+        self.assertGreater(len(smoothie_data), 0)
+        # Assert that at least one recipe contains the test ingredient in its title
+        try:
+            next(filter(lambda recipe: test_ingredient in smoothie_data['ingredient'] == 'Banana', smoothie_data))
+        except StopIteration:
+            self.fail('Test ingredient not in any of the recipe titles')
+
+    # def test_smoothie_data_good(self):
+    #     with mock.patch("builtins.input", side_effect=["banana"]):
+    #         with self.assertEqual(items['banana']):
+    #             smoothie_search()
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
